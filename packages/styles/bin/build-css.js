56017f187a73ab33a1ca5c89505d5b62040860f4
Paul <paulkaspriskie@gmail.com>
2025-09-30 20:09:55 -0400

    feat(styles): implemented css build script and preliminary logic.

---
+++ packages/styles/bin/build-css.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const postcss = require('postcss');
+
+const inputFile = path.resolve(__dirname, '../src/css/index.css');
+const outputDir = path.resolve(__dirname, '../../../dist/moxie-styles');
+const outputFile = path.join(outputDir, 'index.css');
+const outputMapFile = path.join(outputDir, 'index.css.map');
+
+
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir, { recursive: true });
+}
+
+
+fs.readFile(inputFile, (err, css) => {
+  if (err) console.log(err);
+  postcss()
+    .process(css, { from: inputFile, to: outputFile })
+    .then(result => {
+      console.log(result.css);
+      fs.writeFile(outputFile, result.css, (err) => {
+        if (err) console.log(err);
+      });
+      if ( result.map ) {
+        fs.writeFile(outputMapFile, result.map.toString(), () => true)
+      }
+    })
+});
