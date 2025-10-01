b0f0130bce2767bf26b92cb29035e1faaf1865fc
Paul <paulkaspriskie@gmail.com>
2025-10-01 19:34:57 -0400

    build(styles): added cssnano plugin to css-build file.

--- packages/styles/bin/build-css.js
+++ packages/styles/bin/build-css.js
@@ -1,30 +1,24 @@
-const fs = require('fs');
-const path = require('path');
-const autoprefixer = require('autoprefixer');
-const postcss = require('postcss');
+const fs = require("fs");
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const postcss = require("postcss");
+const cssnano = require("cssnano");
 
-const inputFile = path.resolve(__dirname, '../src/css/index.css');
-const outputDir = path.resolve(__dirname, '../../../dist/moxie-styles');
-const outputFile = path.join(outputDir, 'index.css');
-const outputMapFile = path.join(outputDir, 'index.css.map');
-
+const inputFile = path.resolve(__dirname, "../src/css/index.css");
+const outputDir = path.resolve(__dirname, "../../../dist/moxie-styles");
+const outputFile = path.join(outputDir, "index.css");
 
 if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir, { recursive: true });
 }
 
-
 fs.readFile(inputFile, (err, css) => {
   if (err) console.log(err);
-  postcss([autoprefixer])
+  postcss([autoprefixer, cssnano])
     .process(css, { from: inputFile, to: outputFile })
-    .then(result => {
-      console.log(result.css);
+    .then((result) => {
       fs.writeFile(outputFile, result.css, (err) => {
         if (err) console.log(err);
       });
-      if ( result.map ) {
-        fs.writeFile(outputMapFile, result.map.toString(), () => true)
-      }
-    })
+    });
 });
