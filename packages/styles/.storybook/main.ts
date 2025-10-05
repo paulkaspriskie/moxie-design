fcc1b55d8134141d18e8ac23a5a424f5a0ea29a9
Paul <paulkaspriskie@gmail.com>
2025-10-05 11:40:57 -0400

    feature(styles): added rudimentary storybook configuration and dependencies.

---
+++ packages/styles/.storybook/main.ts
@@ -0,0 +1,18 @@
+import type { StorybookConfig } from '@storybook/html-vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y'
+  ],
+  framework: {
+    name: '@storybook/html-vite',
+    options: {}
+  },
+  staticDirs: ['../src']
+};
+
+export default config;
