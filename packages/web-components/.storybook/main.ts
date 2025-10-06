461cfedde3b6f593987d4e7e7573b9c3e6aa619e
Paul <paulkaspriskie@gmail.com>
2025-10-06 18:36:29 -0400

    build(WC): created preliminary storybook configuration & build dependencies.

---
+++ packages/web-components/.storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/web-components-vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials'
+  ],
+  framework: {
+    name: '@storybook/web-components-vite',
+    options: {}
+  },
+  docs: {
+    autodocs: 'tag'
+  },
+  core: {
+    disableTelemetry: true
+  }
+};
+
+export default config;
