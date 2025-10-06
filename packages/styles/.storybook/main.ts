6f2a665eea516741c1188562ec5bec8f1cfe88c8
Paul <paulkaspriskie@gmail.com>
2025-10-06 09:56:44 -0400

    fix(moxie-styles): rolled back storybook to v8.6 to resolve mdx file render issues.

--- packages/styles/.storybook/main.ts
+++ packages/styles/.storybook/main.ts
@@ -3,10 +3,7 @@ import type { StorybookConfig } from '@storybook/html-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y'
+    '@storybook/addon-essentials'
   ],
   framework: {
     name: '@storybook/html-vite',
